f49c7a20e273237665983eaa0530881af534b3d1
brunetto <brunetto.ziosi@gmail.com>
2016-08-31 21:51:30 +0200

    init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+
+func main() {
+	
+	// Get subfolders inside current folder
+	
+	// Start from current folder 
+	
+	// Get local dir name
+	
+	// Scan local dir files
+	
+	// Create array with file struct containing version information, ...
+	
+	// Sort filenames 
+	
+	// Map old names to new names maintaining version information. Take care of possible name conflicts.
+	
+	// Write map file
+	
+	// Rename files
+	
+	// Repeat for subfolders
+	
+	
+	
+}
